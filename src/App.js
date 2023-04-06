70dc6ed5950804a82acc5ff7333f3fcb31dc99b0
marc terre <marc.terre@outlook.de>
2023-04-06 14:29:44 +0200

    setup: gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <main>
+        <Home />
+        <About />
+      </main>
       <Footer />
     </>
   );
