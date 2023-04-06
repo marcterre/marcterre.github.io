fcd688bf001997b3596a51d486a9138d62d193c9
marc terre <marc.terre@outlook.de>
2023-04-06 21:49:32 +0200

    add: Contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </main>
       <Footer />
     </>
