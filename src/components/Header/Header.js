f049adcb9ce5a00f5b2187fe96acc39a44ee83a9
marc terre <marc.terre@outlook.de>
2023-04-06 12:21:09 +0200

    add: Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import "./Header.css";
+
+export default function Header() {
+  const links = [
+    { name: "about me", link: "about" },
+    { name: "projects", link: "projects" },
+    { name: "contact", link: "contact" },
+  ];
+
+  return (
+    <header>
+      <nav className="navigation">
+        <ul className="link-list">
+          {links.map((link) => (
+            <li key={link.name} className="link-list-item">
+              <a href={link.link}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
