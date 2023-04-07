f290892d923335d19749a304925e13d546b9ba07
marc terre <marc.terre@outlook.de>
2023-04-07 23:57:36 +0200

    add: first project box

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,69 @@
+import { useState } from "react";
 import "./Projects.css";
 
 export default function Projects() {
+  const [showDescription, setShowDescription] = useState(false);
+  const currentProjects = [];
+
+  const finishedProjects = [
+    {
+      name: "PocketBuilder",
+      description:
+        "This app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time",
+      techStack: [
+        "Next.js",
+        "React",
+        "Styled components",
+        "Cloudinary",
+        "Figma",
+      ],
+      codeLink: "https://github.com/marcterre/capstone-project",
+      preview: "https://capstone-project-1zduxfvlc-marcterre.vercel.app/",
+    },
+  ];
+
   return (
-    <section className="projects" id="projects">
-      <h2>My Projects</h2>
-      <p>this section is in progress...</p>
-    </section>
+    <>
+      <section className="projects" id="projects">
+        <h2>My Projects</h2>
+        <p>this section is in progress...</p>
+        {currentProjects && (
+          <section>
+            <h3>Currently working on</h3>
+          </section>
+        )}
+        {finishedProjects && (
+          <section>
+            <h3>Finished projects</h3>
+            <div className="project-wrapper">
+              <ul className="project-list">
+                {finishedProjects.map((project) => (
+                  <li key={project.name} className="project-list-item">
+                    <h4>{project.name}</h4>
+                    <button
+                      type="button"
+                      onClick={() => setShowDescription(!showDescription)}
+                    >
+                      {showDescription
+                        ? "hide description"
+                        : "read description"}
+                    </button>
+                    {showDescription && <p>{project.description}</p>}
+                    <div className="link-wrapper">
+                      <a className="link" href={project.codeLink}>
+                        code
+                      </a>
+                      <a className="link" href={project.preview}>
+                        preview
+                      </a>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        )}
+      </section>
+    </>
   );
 }
