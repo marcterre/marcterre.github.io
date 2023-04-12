a9406f143de74c0b17a4892e3af632e599f9f1d2
Marc Terre <marc.terre@outlook.de>
2023-04-12 15:31:35 +0200

    fix: typo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
     {
       name: "PocketBuilder",
       description:
-        "This is my capstone project for the certification as web developmer. The app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
+        "This is my capstone project for the certification as web developer. The app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
       image: CapstoneImage,
       techStack: [
         "Next.js",
@@ -21,7 +21,14 @@ export default function Projects() {
       codeLink: "https://github.com/marcterre/capstone-project",
       preview: "https://capstone-project-1zduxfvlc-marcterre.vercel.app/",
     },
-  ];
+   /* {name: "Weather and Activity App",
+  description: "You want to have an overview of your activities and want to know when its the best weather to do it? Just add your activity.",
+image: "",
+techStack: ["React"],
+codeLink: "https://github.com/marcterre/weather-and-activity-app",
+preview: ""}
+  
+  */];
 
   return (
       <section className="projects" id="projects">
@@ -53,9 +60,9 @@ export default function Projects() {
                     </button>
                      <a className="project-link link" href={project.codeLink}>
                      <SvgIcon variant="link" width="30" color="var(--color)"/> View the code on github </a>
-                      <a className="project-link link" href={project.preview}>
+                     {project.preview && <a className="project-link link" href={project.preview}>
                       <SvgIcon variant="link" width="30" color="var(--color)"/> View the preview in vercel
-                      </a>
+                      </a>}
                     </div>
                   </li>
                 ))}
