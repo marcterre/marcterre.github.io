d2935af8c69a7f5ee068ca5c43ca2d34e99b858a
Marc Terre <marc.terre@outlook.de>
2023-04-12 15:21:33 +0200

    change project overview styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
 import CapstoneImage from "../../assets/pocketBuilderImage.png"
+import SvgIcon from "../../SVGIcon";
 import "./Projects.css";
 
 export default function Projects() {
   const [showDescription, setShowDescription] = useState(false);
-  const currentProjects = [];
-
   const finishedProjects = [
     {
       name: "PocketBuilder",
       description:
-        "This app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
+        "This is my capstone project for the certification as web developmer. The app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
       image: CapstoneImage,
       techStack: [
         "Next.js",
         "React",
-        "Styled components",
         "Cloudinary",
         "Figma",
+        "Styled components",
       ],
       codeLink: "https://github.com/marcterre/capstone-project",
       preview: "https://capstone-project-1zduxfvlc-marcterre.vercel.app/",
@@ -25,28 +24,24 @@ export default function Projects() {
   ];
 
   return (
-    <>
       <section className="projects" id="projects">
         <h2>My Projects</h2>
-        <p>this section is in progress...</p>
-        {currentProjects && (
           <section>
-            <h3>Currently working on</h3>
-          </section>
-        )}
-        {finishedProjects && (
-          <section>
-            <h3>Finished projects</h3>
             <div className="project-wrapper">
               <ul className="project-list">
                 {finishedProjects.map((project) => (
                   <li key={project.name} className="project-list-item">
                     <h4>{project.name}</h4>
-                    <div className="image-wrapper" ><img
+                   <div className="image-wrapper" ><img
                       src={project.image}
                       alt="Not added yet"
                       className="project-image"
-                    ></img></div>
+                    ></img>
+                   {showDescription && <p className="project-description" >{project.description}</p>}
+                    </div>
+                    <ul className="project-tech-list" >{project.techStack.map((tech) => (
+                      <li key={tech} className="project-tech-list-item" >{tech}</li>
+              ))}</ul>
                     <div className="button-link-wrapper" >
                     <button
                       type="button"
@@ -56,22 +51,17 @@ export default function Projects() {
                         ? "hide description"
                         : "read description"}
                     </button>
-                    <div className="link-wrapper">
                      <a className="project-link link" href={project.codeLink}>
-                       View the code on github</a>
+                     <SvgIcon variant="link" width="30" color="var(--color)"/> View the code on github </a>
                       <a className="project-link link" href={project.preview}>
-                        View the preview in vercel
+                      <SvgIcon variant="link" width="30" color="var(--color)"/> View the preview in vercel
                       </a>
-                      </div>
                     </div>
-                    {showDescription && <p className="project-description" >{project.description}</p>}
                   </li>
                 ))}
               </ul>
             </div>
           </section>
-        )}
-      </section>
-    </>
+        </section>
   );
 }
