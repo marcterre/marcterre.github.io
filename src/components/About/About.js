0a6bad1a5faca3945d7f48b7017891af0e4dd522
ripster <marc.terre@outlook.de>
2023-06-22 09:50:55 +0200

    delete sections

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,71 +2,40 @@ import "./About.css";
 import SvgIcon from "../../SVGIcon";
 
 export default function About() {
-  const techStack = [
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "React",
-    "Next.js",
-    "Node.js",
-    "VS Code",
-    "npm",
-    "MongoDB",
-    "git",
-    "shell",
-    "GitHub",
-    "Jotai",
-  ];
+	const techStack = [
+		"JavaScript",
+		"HTML5",
+		"CSS3",
+		"React",
+		"Next.js",
+		"Node.js",
+		"VS Code",
+		"npm",
+		"MongoDB",
+		"git",
+		"shell",
+		"GitHub",
+		"Jotai",
+	];
 
-  const hobbies = [
-    {
-      name: "cooking",
-      icon: <SvgIcon variant="cooking" width="30" color="var(--color)" />,
-    },
-    {
-      name: "reading",
-      icon: <SvgIcon variant="reading" width="30" color="var(--color)" />,
-    },
-    {
-      name: "movies",
-      icon: <SvgIcon variant="movies" width="30" color="var(--color)" />,
-    },
-    {
-      name: "gaming",
-      icon: <SvgIcon variant="gaming" width="30" color="var(--color)" />,
-    },
-  ];
+	const hobbies = [
+		{
+			name: "cooking",
+			icon: <SvgIcon variant="cooking" width="30" color="var(--color)" />,
+		},
+		{
+			name: "reading",
+			icon: <SvgIcon variant="reading" width="30" color="var(--color)" />,
+		},
+		{
+			name: "movies",
+			icon: <SvgIcon variant="movies" width="30" color="var(--color)" />,
+		},
+		{
+			name: "gaming",
+			icon: <SvgIcon variant="gaming" width="30" color="var(--color)" />,
+		},
+	];
 
-  return (
-    <section className="about" id="about">
-      <h2>About me</h2>
-      <article>
-        I live in my self converted camper always looking for a new adventure.
-        One of my biggest dreams - a road trip through Norway.
-        <br />
-        <br />
-        Programming got me hooked during lockdown and i enjoy using my
-        potential. My passion is to create things and there is nothing better
-        than creating dynamic code and responsive websites.
-      </article>
-      <section>
-        <h3>Things i learned and use:</h3>
-        <ul className="tech-list">
-          {techStack.map((techs) => (
-            <li key={techs} className="tech-list-item">
-              {techs}
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section>
-        <h3>Other stuff i like:</h3>
-        <ul className="hobby-list">
-          {hobbies.map((hobby) => (
-            <li key={hobby.name}>{hobby.icon}</li>
-          ))}
-        </ul>
-      </section>
-    </section>
-  );
+	return <section className="about" id="about"></section>;
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,74 +1,34 @@
 import { useState } from "react";
-import CapstoneImage from "../../assets/pocketBuilderImage.png"
+import CapstoneImage from "../../assets/pocketBuilderImage.png";
 import SvgIcon from "../../SVGIcon";
 import "./Projects.css";
 
 export default function Projects() {
-  const [showDescription, setShowDescription] = useState(false);
-  const finishedProjects = [
-    {
-      name: "PocketBuilder",
-      description:
-        "This is my capstone project for the certification as web developer. The app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
-      image: CapstoneImage,
-      techStack: [
-        "Next.js",
-        "React",
-        "Cloudinary",
-        "Figma",
-        "Styled components",
-      ],
-      codeLink: "https://github.com/marcterre/capstone-project",
-      preview: "https://capstone-project-1zduxfvlc-marcterre.vercel.app/",
-    },
-   /* {name: "Weather and Activity App",
+	const [showDescription, setShowDescription] = useState(false);
+	const finishedProjects = [
+		{
+			name: "PocketBuilder",
+			description:
+				"This is my capstone project for the certification as web developer. The app is especially designed for all hobbyists and craftsmen who want to plan new projects and retrieve them at any time.",
+			image: CapstoneImage,
+			techStack: ["Next.js", "React", "Cloudinary", "Figma", "Styled components"],
+			codeLink: "https://github.com/marcterre/capstone-project",
+			preview: "https://capstone-project-1zduxfvlc-marcterre.vercel.app/",
+		},
+		/* {name: "Weather and Activity App",
   description: "You want to have an overview of your activities and want to know when its the best weather to do it? Just add your activity.",
 image: "",
 techStack: ["React"],
 codeLink: "https://github.com/marcterre/weather-and-activity-app",
 preview: ""}
   
-  */];
+  */
+	];
 
-  return (
-      <section className="projects" id="projects">
-        <h2>My Projects</h2>
-          <section>
-            <div className="project-wrapper">
-              <ul className="project-list">
-                {finishedProjects.map((project) => (
-                  <li key={project.name} className="project-list-item">
-                    <h4>{project.name}</h4>
-                   <div className="image-wrapper" ><img
-                      src={project.image}
-                      alt="Not added yet"
-                      className="project-image"
-                    ></img>
-                   {showDescription && <p className="project-description" >{project.description}</p>}
-                    </div>
-                    <ul className="project-tech-list" >{project.techStack.map((tech) => (
-                      <li key={tech} className="project-tech-list-item" >{tech}</li>
-              ))}</ul>
-                    <div className="button-link-wrapper" >
-                    <button
-                      type="button"
-                      onClick={() => setShowDescription(!showDescription)}
-                    >
-                      {showDescription
-                        ? "hide description"
-                        : "read description"}
-                    </button>
-                     <a className="project-link link" href={project.codeLink}>
-                     <SvgIcon variant="link" width="30" color="var(--color)"/> View the code on github </a>
-                     {project.preview && <a className="project-link link" href={project.preview}>
-                      <SvgIcon variant="link" width="30" color="var(--color)"/> View the preview in vercel
-                      </a>}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-        </section>
-  );
+	return (
+		<section className="projects" id="projects">
+			<h2>My Projects</h2>
+			<section></section>
+		</section>
+	);
 }
