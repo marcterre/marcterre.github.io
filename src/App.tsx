c5428c327a724af6505d12a04d4175db917b0b21
marcterre <marc.terre@outlook.de>
2023-06-25 00:19:38 +0200

    add mongo db connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { HeaderSection, ProjectSection } from "./views/HomePageView";
 import { FooterSection } from "./views/HomePageView";
 
 function App() {
+  console.log(process.env.REACT_APP_MONGO_DB_URL);
   return (
     <>
       <HeaderSection />
